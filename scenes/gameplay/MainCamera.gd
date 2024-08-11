82b1cc0b7f902ab20ecd9aaf2764071888099415
atumemot <atumemot@gmail.com>
2024-08-10 22:51:32 -0700

    camera options

--- scenes/gameplay/MainCamera.gd
+++ scenes/gameplay/MainCamera.gd
@@ -4,6 +4,21 @@ func _ready():
 	position = get_viewport().size / 2
 
 func _process(delta):
-	pass
-	position = $"../Worm/Head".position
+	# pass
+	# static center of screen: just do nothing
+	
+	# static camera bias towards mouse
+	# var offset = get_viewport().get_mouse_position() - get_viewport().size/2 + position
+	# var target = (((get_viewport().size / 2) - offset) * 0.95) + offset
+	# position = position.cubic_interpolate(target, position, target, 0.3)
+	
+	#position = get_viewport().size / 2
+	
+	# follow worm
+	# position = $"../Worm/Head".position
+	
+	# bias towards mouse
+	var offset = get_viewport().get_mouse_position() - get_viewport().size/2 + position
+	var target = (($"../Worm/Head".position - offset) * 0.9) + offset
+	position = position.cubic_interpolate(target, position, target, 0.3)
 	
