b5790cb20a9789406b4df9e2fc8e8329fd49b91b
atumemot <atumemot@gmail.com>
2024-08-10 19:45:09 -0700

    collider fix attempts

--- scenes/captaintoto/WormCollider.gd
+++ scenes/captaintoto/WormCollider.gd
@@ -10,23 +10,23 @@ signal on_hit()
 onready var shape: CollisionPolygon2D = $Shape
 
 func build_collider(line: PoolVector2Array):
-	# remove overlapping points from line because it prevents the polygon from being created right
-	for i in line.size():
-		while i > 0 and i < line.size() - 1:
-			if line.size() == 1:
-				break
-			if line[i] == line[i - 1]:
-				line.remove(i)
-			else:
-				break
-			
+	# result is an array of Array[Vector2]
 	var result = Geometry.offset_polyline_2d(line, width)
+	
+	# if result is more than a single shape, then worm is overlapping itself
 	if result.size() > 1:
 		emit_signal("on_hit_self")
 	else:
+		# remove overlapping points from shape
+		for i in result[0].size():
+			for j in range(i + 1, result[0].size()):
+				if j >= result[0].size():
+					break
+				if (result[0][j] - result[0][i]).length() < 0.5:
+					result[0].remove(j)
+						
+		# set collider shape, error happens here
 		shape.polygon = PoolVector2Array(result[0])
-	# for point in polygon:
-	# 	print(point)
 	
 func _physics_process(delta):
 	var areas = get_overlapping_areas()
