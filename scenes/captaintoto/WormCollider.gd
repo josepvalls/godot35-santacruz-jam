b496bfe1f910afc512d22f5eb09eaef3d7c9def3
atumemot <atumemot@gmail.com>
2024-08-14 21:17:32 -0700

    added other levels

--- scenes/captaintoto/WormCollider.gd
+++ scenes/captaintoto/WormCollider.gd
@@ -12,14 +12,19 @@ signal on_hit()
 onready var shape: CollisionPolygon2D = $Shape
 
 func build_collider(line_: PoolVector2Array):
-	if tick % update_freq != 0: return
+	# if tick % update_freq != 0: return
 	var line = Array(line_)
+	
+	# sub sample line points
 	var result = null
 	while not result:
 		line = line.slice(0, len(line), 3)
 		result = Geometry.offset_polyline_2d(line, width)
+		
 	if result.size() > 1:
 		emit_signal("on_hit_self")
+		
+	# remove duplicates
 	var path := Array()
 	for point in result[0]:
 		if path.size() == 0 or path[path.size() - 1] != point:
--- scenes/gameplay/Background.gd
+++ scenes/gameplay/Background.gd
@@ -5,31 +5,23 @@ export var num_homogeneous := 0
 export var size_w := 2048
 export var size_h := 2048
 export var offset := Vector2.ZERO
-export var color_r_mult := 1.0
-export var color_g_mult := 0.3
+export var color_range_lo := 0.4
+export var color_range_hi := 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var leaves = $Leaves.get_children()
 	for _i in num_random:
 		var leaf = leaves[randi() % len(leaves)].duplicate()
 		leaf.position = Vector2(randf()*size_w, randf()*size_h) - Vector2(size_w,size_h)/2 + Vector2(0, rand_range(offset.x, offset.y))
-		leaf.rotation = randf()*PI*2
-		var rand_brightness = rand_range(0.4, 1)
-		leaf.modulate = Color(rand_brightness,rand_brightness,rand_brightness)
+		leaf.rotation = randf()*360
+		var rand_brightness = rand_range(color_range_lo, color_range_hi)
+		leaf.modulate *= Color(rand_brightness,rand_brightness,rand_brightness)
 		$Leaves.add_child(leaf)
-
-	for _i in num_random:
-		var leaf = leaves[randi() % len(leaves)].duplicate()
-		leaf.position = Vector2(randf()*size_w/2, randf()*size_h/2) - Vector2(size_w,size_h)/4 + Vector2(0, rand_range(offset.x, offset.y))
-		var rand_brightness = rand_range(0.4, 1)
-		leaf.modulate = Color(rand_brightness,rand_brightness,rand_brightness)
-		$Leaves.add_child(leaf)
-
 
 	for _i in num_homogeneous:
 		var leaf = leaves[randi() % len(leaves)].duplicate()
 		leaf.position = Vector2(1.0 * _i * size_w/num_homogeneous, 0) - Vector2(size_w,size_h)/2 + Vector2(0, rand_range(offset.x, offset.y))
-		leaf.rotation = randf()*PI*2
-		var rand_brightness = rand_range(0.4, 1)
-		leaf.modulate = Color(rand_brightness,rand_brightness,rand_brightness)
+		leaf.rotation = randf()*360
+		var rand_brightness = rand_range(color_range_lo, color_range_hi)
+		leaf.modulate *= Color(rand_brightness,rand_brightness,rand_brightness)
 		$Leaves.add_child(leaf)
